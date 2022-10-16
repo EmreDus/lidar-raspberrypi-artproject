b9e4b8d16fa4411672668b4fc8bbb346ff9a925b
Muhammet Emre DUŞ <101596046+EmreDus@users.noreply.github.com>
2022-10-16 22:32:22 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install numpy
+pip3 install rplidar-roboticia
+pip3 install audioplayer
+sudo apt install python3-gst-1.0
+sudo apt-get install libsdl-ttf2.0-0
+pip install minimalmodbus
